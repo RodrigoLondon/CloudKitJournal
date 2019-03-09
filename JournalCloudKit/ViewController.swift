7c79c4aa66875da0e5fe75b2e670691b7f5c4a08
Lewis Jones <lewisjones@unknown>
2019-03-09 13:52:30 +0000

    Initial Commit

---
+++ JournalCloudKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JournalCloudKit
+//
+//  Created by Lewis Jones on 09/03/2019.
+//  Copyright © 2019 Rodrigo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
